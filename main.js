42f0db5c5f845417a8d3fc1f3d038c26c4fdcfd3
Lin Jingbin <linjingbin@proudsmart.com>
2018-12-21 17:49:53 +0800

    update ignore

--- main.js
+++
@@ -1,4 +0,0 @@
-import { renderAll } from "-!./lib/files-extractor.js!./lib/angular-loader.js";
-let module = angular.module("app", ['ngRoute']);
-renderAll(module);
-window.angular.bootstrap(document.body, [module.name]);
